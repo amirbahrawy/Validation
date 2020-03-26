2089b297977f5a67d8e03956f784f86a770e01f0
Abdelaziz <eng.3bdel3ziz@gmail.com>
2020-03-26 20:34:25 +0200

    some html and css edits added variables in js

---
+++ validation.js
@@ -0,0 +1,8 @@
+var Name= document.getElementById('names'),
+    Phone=document.getElementById('phone'),
+    Pass=document.getElementById('pass'),
+    PassConfirm=document.getElementById('confirm_pass'),
+    Err0r=document.getElementById('error'),
+    Check=document.getElementById('check'),
+    Submit=document.getElementById('submit');
+
